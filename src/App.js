6f9c6b8c0ad75320ff28a005a179b108fb0c0a7a
Filippo Barcellos <barcellos.filippo@gmail.com>
2021-02-12 21:20:50 +0000

    resolve [issue 2]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import './App.css';
+
+import Filters from './features/filters/Filters';
+import AddTodo from './features/todos/AddTodo';
+import TodoList from './features/todos/TodoList';
+
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <h1>todolist</h1>
+      <Filters />
+      <AddTodo />
+      <TodoList />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import App from './App';
+import rootReducer from './reducers';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
