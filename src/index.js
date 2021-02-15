97d337713644f6048e5e07a34ab96a337eeb27e2
Filippo Barcellos <barcellos.filippo@gmail.com>
2021-02-15 21:56:36 +0000

    add test render todo component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
 import App from './App';
-import rootReducer from './reducers';
-
-const store = configureStore({
-  reducer: rootReducer,
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
